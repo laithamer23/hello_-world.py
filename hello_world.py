b00ce473c43f1b3cf2c767395385ea6e9d7b8219
laithamer23 <165911446+laithamer23@users.noreply.github.com>
2024-04-28 13:21:56 +0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print( "hello world")
+
+name= "noeel"
+print("hello",name,"!")
+
+print("hello "+ name+"!")
+
+name=42
+print("hello",name,"!")
+
+#print("hello"+name+"!") # this one is an error
+
+fave_food1="suchi"
+fave_food2="pizza"
+print("i love to eat {} and {}".format(fave_food1,fave_food2))
+print(f"i love to eat {fave_food1} and {fave_food2}")
